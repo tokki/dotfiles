f942380c561c3664da602cb299cd8bd5fae59e11
tokki <tokki@live.cn>
2022-09-19 17:16:54 +0800

    m

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"html", "css", "vim", "lua", "python", "go"},
+  ensure_installed = {"html", "css", "vim", "lua", "python", "go", "vim", "javascript", "yaml", "json"},
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -2,17 +2,16 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function()
 	use 'wbthomason/packer.nvim'
-
 	use 'sainnhe/sonokai'
-	use 'nvim-treesitter/nvim-treesitter'
-	use 'akinsho/bufferline.nvim'
-	use 'kyazdani42/nvim-tree.lua'
+	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 	use 'neovim/nvim-lspconfig'
 	use 'hrsh7th/nvim-cmp'
 	use 'hrsh7th/cmp-nvim-lsp'
-	use 'L3MON4D3/LuaSnip'
-	use 'saadparwaiz1/cmp_luasnip'
-
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/cmp-cmdline'
+	use 'hrsh7th/cmp-vsnip'
+	use 'hrsh7th/vim-vsnip'
 end)
 
 
